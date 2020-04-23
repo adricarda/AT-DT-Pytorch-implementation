b08238a847df5c8657085cc2b5da3767123db4d9
adricarda <cardace6@gmail.com>
2020-04-23 12:53:10 +0000

    change defaul training list to carla

--- launchers/segmentation_train.sh
+++ launchers/segmentation_train.sh
@@ -5,5 +5,5 @@ python3 ../train.py \
     --model_dir '/content/drive/My Drive/Depth/experiments/segmentation_resnet50' \
     --checkpoint_dir '/content/drive/My Drive/Depth/experiments/segmentation_resnet50' \
     --tensorboard_dir '/content/drive/My Drive/Depth/experiments/segmentation_resnet50/tensorboard' \
-    --txt_train '/content/drive/My Drive/atdt/input_list_train_cityscapes.txt' \
-    --txt_val '/content/drive/My Drive/atdt/input_list_val_cityscapes.txt'
+    --txt_train '/content/drive/My Drive/atdt/input_list_train_carla.txt' \
+    --txt_val '/content/drive/My Drive/atdt/input_list_val_carla.txt'
