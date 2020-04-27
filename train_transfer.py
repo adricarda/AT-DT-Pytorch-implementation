07beea02278f6dd9342cf7d4e45d3f48d2553581
adricarda <cardace6@gmail.com>
2020-04-27 12:46:02 +0000

    training backbone

--- train_transfer.py
+++ train_transfer.py
@@ -40,19 +40,15 @@ parser.add_argument('--checkpoint_dir_transfer', default="experiments/transfer_b
 parser.add_argument('--tensorboard_dir', default="experiments/transfer_baseline/tensorboard",
                     help="Directory for Tensorboard data")
 parser.add_argument('--txt_train', default='/content/drive/My Drive/atdt/input_list_train_carla.txt',
-                    help="Txt file containing path to training images for carla")
-parser.add_argument('--txt_train_cs', default='/content/drive/My Drive/atdt/input_list_train_cityscapes.txt',
-                    help="Txt file containing path to training images for cityscapes")
+                    help="Txt file containing path to training images")
 parser.add_argument('--txt_val_source', default='/content/drive/My Drive/atdt/input_list_val_carla.txt',
                     help="Txt file containing path to validation images source dataset")
 parser.add_argument('--txt_val_target', default='/content/drive/My Drive/atdt/input_list_val_cityscapes.txt',
                     help="Txt file containing path to validation images target dataset")
 
-
 def get_lr(opt):
     for param_group in opt.param_groups:
         return param_group['lr']
-
 
 def inference(model, batch):
     model.eval()
@@ -61,156 +57,147 @@ def inference(model, batch):
     return y_pred
 
 
-def train_epoch(model_source, model_target, transfer, 
-                train_dl_all, train_dl_depth_target, val_dl_source_all, val_dl_target, 
-                loss_fn1, loss_fn2, opt1=None, opt2=None, opt3=None, lr_scheduler=None, metrics=None, params=None):
-    # running_loss = utils.RunningAverage()
-    # num_batches = len(dataset_dl)
+def train_epoch(source_encoder, target_encoder, transfer, loss_fn, dataset_dl, opt=None, lr_scheduler=None, metrics=None, params=None):
+    running_loss = utils.RunningAverage()
+    num_batches = len(dataset_dl)
 
-    # if metrics is not None:
-    #     for metric_name, metric in metrics.items():
-    #         metric.reset()
+    if metrics is not None:
+        for metric_name, metric in metrics.items():
+            metric.reset()
 
-    # for (xb, _) in tqdm(dataset_dl):
-    #     xb = xb.to(params.device)
-    #     output_source_encoder = source_encoder(xb)['out']
-    #     output_target_encoder = target_encoder(xb)['out']
-    #     output_transfer = transfer(output_source_encoder)
+    for (xb, _) in tqdm(dataset_dl):
+        xb = xb.to(params.device)
+        output_source_encoder = source_encoder(xb)['out']
+        output_target_encoder = target_encoder(xb)['out']
+        output_transfer = transfer(output_source_encoder)
 
-    #     loss_b = loss_fn(output_transfer, output_target_encoder)
+        loss_b = loss_fn(output_transfer, output_target_encoder)
 
-    #     if opt is not None:
-    #         opt.zero_grad()
-    #         loss_b.backward()
-    #         opt.step()
+        if opt is not None:
+            opt.zero_grad()
+            loss_b.backward()
+            opt.step()
 
-    #     if lr_scheduler is not None:
-    #         lr_scheduler.step()
+        if lr_scheduler is not None:
+            lr_scheduler.step()
 
-    #     running_loss.update(loss_b.item())
+        running_loss.update(loss_b.item())
+        
+        # if metrics is not None:
+        #     for metric_name, metric in metrics.items():
+        #         metric.add(output.detach(), yb)
 
-    #     # if metrics is not None:
-    #     #     for metric_name, metric in metrics.items():
-    #     #         metric.add(output.detach(), yb)
-
-    # if metrics is not None:
-    #     metrics_results = OrderedDict({})
-    #     for metric_name, metric in metrics.items():
-    #         metrics_results[metric_name] = metric.value()
-    #     return running_loss(), metrics_results
-    # else:
-    #     return running_loss(), None
-    pass
+    if metrics is not None:
+        metrics_results = OrderedDict({})
+        for metric_name, metric in metrics.items():
+            metrics_results[metric_name] = metric.value()
+        return running_loss(), metrics_results
+    else:
+        return running_loss(), None
 
 
-def train_and_evaluate(model_source, model_target, transfer, train_dl_all, train_dl_depth_target, val_dl_source_all, val_dl_target, 
-                        opt1, opt2, opt3, loss_fn1, loss_fn2, metrics, params,
-                        lr_scheduler1, lr_scheduler2, lr_scheduler3,
-                        checkpoint_dir, ckpt_filename, log_dir, writer):
+def train_and_evaluate(model_source, model_target, transfer, train_dl, val_dl_source, val_dl_target, opt, loss_fn, metrics, params,
+                       lr_scheduler, checkpoint_dir, ckpt_filename, log_dir, writer):
 
     ckpt_file_path = os.path.join(checkpoint_dir, ckpt_filename)
     best_value = -float('inf')
     early_stopping = utils.EarlyStopping(patience=10, verbose=True)
     start_epoch = 0
 
-    # batch_sample_source, batch_gt_source = next(iter(val_dl_source))
-    # batch_sample_target, batch_gt_target = next(iter(val_dl_target))
+    batch_sample_source, batch_gt_source = next(iter(val_dl_source))
+    batch_sample_target, batch_gt_target = next(iter(val_dl_target))
 
-    # if os.path.exists(ckpt_file_path):
-    #     model, opt, lr_scheduler, start_epoch, best_value = utils.load_checkpoint(transfer, opt, lr_scheduler,
-    #                                                             start_epoch, False, best_value, checkpoint_dir, ckpt_filename)
-    #     print("=> loaded transfer checkpoint form {} (epoch {})".format(
-    #         ckpt_file_path, start_epoch))
-    # else:
-    #     print("=> Initializing from scratch")
+    if os.path.exists(ckpt_file_path):
+        model, opt, lr_scheduler, start_epoch, best_value = utils.load_checkpoint(transfer, opt, lr_scheduler,
+                                                                start_epoch, False, best_value, checkpoint_dir, ckpt_filename)
+        print("=> loaded transfer checkpoint form {} (epoch {})".format(
+            ckpt_file_path, start_epoch))
+    else:
+        print("=> Initializing from scratch")
 
-    # source_encoder = model_source.backbone
-    # target_encoder = model_target.backbone
-    # target_decoder = model_target.classifier
+    source_encoder = model_source.backbone
+    target_encoder = model_target.backbone
+    target_decoder = model_target.classifier
 
-    # adpative_model = get_adaptive_network(source_encoder, transfer, target_decoder)
+    adpative_model = get_adaptive_network(source_encoder, transfer, target_decoder)
 
     for epoch in range(start_epoch, params.num_epochs):
         # Run one epoch
-        current_lr = get_lr(opt1)
-        logging.info('Epoch {}/{}, current lr={}'.format(epoch,
-                                                         params.num_epochs-1, current_lr))
+        current_lr = get_lr(opt)
+        logging.info('Epoch {}/{}, current lr={}'.format(epoch, params.num_epochs-1, current_lr))
         writer.add_scalar('Learning_rate', current_lr, epoch)
 
         transfer.train()
-        train_loss, train_metrics = train_epoch(model_source, model_target, transfer, 
-                                                train_dl_all, train_dl_depth_target, val_dl_source_all, val_dl_target, 
-                                                opt1, opt2, opt3, loss_fn1, loss_fn2,
-                                                lr_scheduler1, lr_scheduler2, lr_scheduler3, params=params)
-
-        # transfer.eval()
-        # val_loss_source, _ = train_epoch(
-        #     source_encoder, target_encoder, transfer, loss_fn, val_dl_source, params=params)
+        train_loss, train_metrics = train_epoch(
+            source_encoder, target_encoder, transfer, loss_fn, train_dl, opt, lr_scheduler, params=params)
+    
+        transfer.eval()
+        val_loss_source, _ = train_epoch(
+            source_encoder, target_encoder, transfer, loss_fn, val_dl_source, params=params)
 
         # Evaluate for one epoch on validation set
-        # _, val_metrics_source = evaluate(
-        #     adpative_model, None, val_dl_source, metrics=metrics, params=params)
+        _, val_metrics_source = evaluate(
+            adpative_model, None, val_dl_source, metrics=metrics, params=params)
 
-        # _, val_metrics_target = evaluate(
-        #     adpative_model, None, val_dl_target, metrics=metrics, params=params)
+        _, val_metrics_target = evaluate(
+            adpative_model, None, val_dl_target, metrics=metrics, params=params)
 
-        # writer.add_scalars('Loss', {
-        #     # 'Training': train_loss,
-        #     'Validation': val_loss_source,
-        # }, epoch)
+        writer.add_scalars('Loss', {
+            'Training': train_loss,
+            'Validation': val_loss_source,
+        }, epoch)
 
-        # for (val_metric_name_s, val_metric_results_s), (val_metric_name_t, val_metric_results_t) in zip(val_metrics_source.items(), val_metrics_target.items()):
-        #     writer.add_scalars(val_metric_name_s, {
-        #         'Validation_source': val_metric_results_s[0],
-        #         'Validation_target': val_metric_results_t[0],
-        #     }, epoch)
+        for (val_metric_name_s, val_metric_results_s), (val_metric_name_t, val_metric_results_t) in zip(val_metrics_source.items(), val_metrics_target.items()):
+            writer.add_scalars(val_metric_name_s, {
+                'Validation_source': val_metric_results_s[0],
+                'Validation_target': val_metric_results_t[0],
+            }, epoch)
 
-        # if epoch % 5 == 0 or epoch==params.num_epochs-1:
-        #     predictions = inference(adpative_model, batch_sample_source)
-        #     plot = train_dl.dataset.get_predictions_plot(
-        #         batch_sample_source, predictions.cpu(), batch_gt_source)
-        #     writer.add_image('Predictions_source', plot, epoch, dataformats='HWC')
+        if epoch % 5 == 0 or epoch==params.num_epochs-1:
+            predictions = inference(adpative_model, batch_sample_source)
+            plot = train_dl.dataset.get_predictions_plot(
+                batch_sample_source, predictions.cpu(), batch_gt_source)
+            writer.add_image('Predictions_source', plot, epoch, dataformats='HWC')
 
-        #     predictions = inference(adpative_model, batch_sample_target)
-        #     plot = train_dl.dataset.get_predictions_plot(
-        #         batch_sample_target, predictions.cpu(), batch_gt_target)
-        #     writer.add_image('Predictions_target', plot, epoch, dataformats='HWC')
+            predictions = inference(adpative_model, batch_sample_target)
+            plot = train_dl.dataset.get_predictions_plot(
+                batch_sample_target, predictions.cpu(), batch_gt_target)
+            writer.add_image('Predictions_target', plot, epoch, dataformats='HWC')
 
-        # current_value = list(val_metrics_source.values())[0][0]
-        # is_best = current_value >= best_value
+        current_value = list(val_metrics_source.values())[0][0]
+        is_best = current_value >= best_value
 
-        # # If best_eval, best_save_path
-        # if is_best:
-        #     logging.info("- Found new best accuracy")
-        #     best_value = current_value
-        #     # Save best val metrics in a json file in the model directory
-        #     best_json_path = os.path.join(
-        #         log_dir, "metrics_val_best_weights.json")
-        #     utils.save_dict_to_json(val_metrics_source, best_json_path)
-        #     utils.save_dict_to_json(val_metrics_target, best_json_path)
+        # If best_eval, best_save_path
+        if is_best:
+            logging.info("- Found new best accuracy")
+            best_value = current_value
+            # Save best val metrics in a json file in the model directory
+            best_json_path = os.path.join(
+                log_dir, "metrics_val_best_weights.json")
+            utils.save_dict_to_json(val_metrics_source, best_json_path)
+            utils.save_dict_to_json(val_metrics_target, best_json_path)
 
-        # # Save weights
-        # utils.save_checkpoint({'epoch': epoch + 1,
-        #                        'state_dict': transfer.state_dict(),
-        #                        'optim_dict': opt.state_dict(),
-        #                        'scheduler_dict': lr_scheduler.state_dict(),
-        #                        'best_value': best_value},
-        #                       is_best=is_best,
-        #                       ckpt_dir=checkpoint_dir,
-        #                       filename=ckpt_filename)
+        # Save weights
+        utils.save_checkpoint({'epoch': epoch + 1,
+                               'state_dict': transfer.state_dict(),
+                               'optim_dict': opt.state_dict(),
+                               'scheduler_dict': lr_scheduler.state_dict(),
+                               'best_value': best_value},
+                              is_best=is_best,
+                              ckpt_dir=checkpoint_dir,
+                              filename=ckpt_filename)
 
-        # logging.info("\ntrain loss: %.3f, val loss: %.3f" %
-        #              (train_loss, val_loss_source))
-
-        # for (val_metric_name_s, val_metric_results_s), (val_metric_name_t, val_metric_results_t) in zip(val_metrics_source.items(), val_metrics_target.items()):
-        #     logging.info("source %s: %.3f, target %s: %.3f" % (val_metric_name_s, val_metric_results_s[0], val_metric_name_t, val_metric_results_t[0]))
+        logging.info("\ntrain loss: %.3f, val loss: %.3f" %
+                     (train_loss, val_loss_source))
+        
+        for (val_metric_name_s, val_metric_results_s), (val_metric_name_t, val_metric_results_t) in zip(val_metrics_source.items(), val_metrics_target.items()):
+            logging.info("source %s: %.3f, target %s: %.3f" % (val_metric_name_s, val_metric_results_s[0], val_metric_name_t, val_metric_results_t[0]))
         logging.info("-"*20)
 
-        # early_stopping(val_loss_source)
-        # if early_stopping.early_stop:
-        #     logging.info("Early stopping")
-        #     break
-
+        early_stopping(val_loss_source)
+        if early_stopping.early_stop:
+            logging.info("Early stopping")
+            break
 
 if __name__ == '__main__':
 
@@ -261,14 +248,11 @@ if __name__ == '__main__':
 
     # fetch dataloaders
     params_transfer.encoding = params_transfer.encoding_source
-    train_dl_all = dataloader.fetch_dataloader(
-        args.data_dir, args.txt_train_carla, 'train', params_transfer, sem_depth=True)
+    train_dl = dataloader.fetch_dataloader(
+        args.data_dir, args.txt_train, 'train', params_transfer)
 
-    train_dl_depth_target = dataloader.fetch_dataloader(
-        args.data_dir, args.txt_train_cs, 'train', params_source)
-
-    val_dl_source_all = dataloader.fetch_dataloader(
-        args.data_dir, args.txt_val_source, 'val', params_transfer, sem_depth=True)
+    val_dl_source = dataloader.fetch_dataloader(
+        args.data_dir, args.txt_val_source, 'val', params_transfer)
 
     params_transfer.encoding = params_transfer.encoding_target
     val_dl_target = dataloader.fetch_dataloader(
@@ -281,62 +265,42 @@ if __name__ == '__main__':
     model_target = get_network(params_target).to(params_transfer.device)
     transfer = get_transfer(params_transfer).to(params_transfer.device)
 
-    # load source and target model before training and extract backbones
-    ckpt_source_file_path = os.path.join(
-        args.checkpoint_dir_source, best_ckpt_filename)
+    #load source and target model before training and extract backbones
+    ckpt_source_file_path = os.path.join(args.checkpoint_dir_source, best_ckpt_filename)
     print(ckpt_source_file_path)
     if os.path.exists(ckpt_source_file_path):
-        model_source = utils.load_checkpoint(
-            model_source, ckpt_dir=args.checkpoint_dir_source, filename=best_ckpt_filename, is_best=True)[0]
-        print("=> loaded source model checkpoint form {}".format(
-            ckpt_source_file_path))
+        model_source = utils.load_checkpoint(model_source, ckpt_dir=args.checkpoint_dir_source, filename=best_ckpt_filename, is_best=True)[0]
+        print("=> loaded source model checkpoint form {}".format(ckpt_source_file_path))
     else:
         print("=> Initializing source model from scratch")
-    ckpt_target_file_path = os.path.join(
-        args.checkpoint_dir_target, best_ckpt_filename)
+    ckpt_target_file_path = os.path.join(args.checkpoint_dir_target, best_ckpt_filename)
     if os.path.exists(ckpt_target_file_path):
-        model_target = utils.load_checkpoint(
-            model_target, ckpt_dir=args.checkpoint_dir_target, filename=best_ckpt_filename, is_best=True)[0]
-        print("=> loaded target model checkpoint form {}".format(
-            ckpt_target_file_path))
+        model_target = utils.load_checkpoint(model_target, ckpt_dir=args.checkpoint_dir_target, filename=best_ckpt_filename, is_best=True)[0]
+        print("=> loaded target model checkpoint form {}".format(ckpt_target_file_path))
     else:
         print("=> Initializing target model from scratch")
+    
+    for p in model_source.parameters():
+        p.requires_grad = False
+    model_source.eval()
 
-    # for p in model_source.parameters():
-    #     p.requires_grad = False
-    # model_source.eval()
+    for p in model_target.parameters():
+        p.requires_grad = False    
+    model_target.eval()
 
-    # for p in model_target.parameters():
-    #     p.requires_grad = False
-    # model_target.eval()
+    opt = optim.AdamW(transfer.parameters(), lr=params_transfer.learning_rate)
+    lr_scheduler = torch.optim.lr_scheduler.OneCycleLR(
+        opt, max_lr=params_transfer.learning_rate, steps_per_epoch=len(train_dl), epochs=params_transfer.num_epochs, div_factor=20)
 
-    opt1 = optim.AdamW(transfer.parameters(), lr=params_transfer.learning_rate)
-    lr_scheduler1 = torch.optim.lr_scheduler.OneCycleLR(
-        opt1, max_lr=params_transfer.learning_rate, steps_per_epoch=len(train_dl_all), epochs=params_transfer.num_epochs, div_factor=20)
-
-    opt2 = optim.AdamW(model_source.parameters(), lr=params_source.learning_rate)
-    lr_scheduler2 = torch.optim.lr_scheduler.OneCycleLR(
-        opt2, max_lr=params_source.learning_rate, steps_per_epoch=len(train_dl_all), epochs=params_transfer.num_epochs, div_factor=20)    
-
-    opt3 = optim.AdamW(model_target.parameters(), lr=params_target.learning_rate)
-    lr_scheduler3 = torch.optim.lr_scheduler.OneCycleLR(
-        opt3, max_lr=params_target.learning_rate, steps_per_epoch=len(train_dl_all), epochs=params_transfer.num_epochs, div_factor=20)    
-        
     # fetch loss function and metrics
-    loss_fn1 = get_loss_fn(params_transfer)
-    loss_fn2 = get_loss_fn(params_source)
-
+    loss_fn = get_loss_fn(params_transfer)
     # num_classes+1 for background.
     metrics = OrderedDict({})
     for metric in params_transfer.metrics:
         metrics[metric] = get_metrics(metric, params_transfer)
 
     # Train the model
-    logging.info("Starting training for {} epoch(s)".format(
-        params_transfer.num_epochs))
+    logging.info("Starting training for {} epoch(s)".format(params_transfer.num_epochs))
 
-    train_and_evaluate(model_source, model_target, transfer, 
-                        train_dl_all, train_dl_depth_target, val_dl_source_all, val_dl_target, 
-                        opt1, opt2, opt3, loss_fn1, loss_fn2, metrics, params_transfer, 
-                        lr_scheduler1, lr_scheduler2, lr_scheduler3, 
-                        args.checkpoint_dir_transfer, ckpt_filename, log_dir, writer)
+    train_and_evaluate(model_source, model_target, transfer, train_dl, val_dl_source, val_dl_target, opt, loss_fn, metrics,
+                       params_transfer, lr_scheduler, args.checkpoint_dir_transfer, ckpt_filename, log_dir, writer)
